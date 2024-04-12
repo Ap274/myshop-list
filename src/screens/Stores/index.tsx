76023f80d02e3c871c5b6066c75226f43a093347
Ap274 <aldopiresaps@gmail.com>
2024-04-12 18:39:46 -0300

    feat: organizing basic structure of the application

---
+++ src/screens/Stores/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+import { Container, Title } from "./styles";
+
+export function Stores() {
+    return (
+        <Container>
+            <Title>Hello World!</Title>
+            <Title>Hey Aldo!</Title>
+        </Container>
+    )
+}
