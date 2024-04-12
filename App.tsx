1701e3ee2c99d30502283340396f642bd41f8621
Ap274 <aldopiresaps@gmail.com>
2024-04-12 19:03:38 -0300

    feat: implement Loading component and stylizing StatusBar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Quicksand_400Regular, Quicksand_700Bold } from '@expo-google-fonts/quicksand';
+import { StatusBar } from 'react-native';
+
+import theme from 'src/theme';
 
 import { Stores } from '@screens/Stores';
 
-import theme from 'src/theme';
-import { ActivityIndicator } from 'react-native';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Quicksand_400Regular, Quicksand_700Bold });
@@ -12,7 +14,13 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Stores/> : <ActivityIndicator /> }
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      { fontsLoaded ? <Stores/> : <Loading /> }
     </ThemeProvider>
   );
 }
