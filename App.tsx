b5612e4994ac33f70310f994cd39559c0444d904
Ap274 <aldopiresaps@gmail.com>
2024-04-12 18:49:31 -0300

    feat: loading font styles from expo-google-fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Quicksand_400Regular, Quicksand_700Bold } from '@expo-google-fonts/quicksand';
 
 import { Stores } from '@screens/Stores';
 
 import theme from 'src/theme';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Quicksand_400Regular, Quicksand_700Bold });
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Stores />
+      { fontsLoaded ? <Stores/> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
