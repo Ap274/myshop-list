322f8c19b18790c3c4595a514988251bda75f1a0
Ap274 <aldopiresaps@gmail.com>
2024-04-15 16:30:34 -0300

    feat: create basic structure of Header component

--- src/screens/Stores/styles.ts
+++ src/screens/Stores/styles.ts
@@ -3,8 +3,7 @@ import styled from "styled-components/native";
 export const Container = styled.View`
     flex: 1;
     background-color: ${({theme}) => theme.colors.gray_600};
-    justify-content: center;
-    align-items: center;
+    padding: 30px;
 `;
 
 export const Title = styled.Text`
