cc857c003c3110e6d75f68477584fdcef8522300
Ap274 <aldopiresaps@gmail.com>
2024-04-15 17:25:01 -0300

    feat: create ShopCard component

---
+++ src/components/ShopCard/styles.ts
@@ -0,0 +1,34 @@
+import { ShoppingCartSimple } from "phosphor-react-native";
+import { TouchableOpacity } from "react-native";
+import styled, { css } from "styled-components/native";
+
+export const Container = styled(TouchableOpacity)`
+    width: 100%;
+    height: 90px;
+
+    background-color: ${({theme}) => theme.colors.gray_500};
+    border-radius: 6px;
+
+    flex-direction: row;
+    align-items: center;
+
+    padding: 24px;
+    margin-bottom: 12px;
+`;
+
+export const Title = styled.Text`
+    ${({theme}) => css`
+        font-size: ${theme.font_size.md}px;
+        font-family: ${theme.font_family.regular};
+        color: ${theme.colors.gray_200};
+    `}
+`;
+
+export const Icon = styled(ShoppingCartSimple).attrs(({theme}) => ({
+    size: 32,
+    color: theme.colors.green_700,
+    weight: 'fill'
+}))`
+    margin: 7px;
+`;
+
--- src/screens/Stores/index.tsx
+++ src/screens/Stores/index.tsx
@@ -1,10 +1,28 @@
 import { Header } from "@components/Header";
+
 import { Container } from "./styles";
+import { Caption } from "@components/Caption";
+import { ShopCard } from "@components/ShopCard";
 
 export function Stores() {
     return (
         <Container>
             <Header />
+
+            <Caption 
+                title="Shopping"
+                subtitle="Things to buy :D"
+            />
+
+            <ShopCard 
+                title="Angeloni"
+            />
+            <ShopCard 
+                title="Giassi"
+            />
+            <ShopCard 
+                title="Hortifruti"
+            />
         </Container>
     )
 }
