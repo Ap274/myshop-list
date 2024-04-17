ed1f124e63e024ec02371196987c70b5fcd37fc7
Ap274 <aldopiresaps@gmail.com>
2024-04-17 18:56:47 -0300

    feat: finish stylization

--- src/screens/Items/styles.ts
+++ src/screens/Items/styles.ts
@@ -4,7 +4,7 @@ import styled, { css } from "styled-components/native";
 export const Container = styled(SafeAreaView)`
     flex: 1;
     background-color: ${({theme}) => theme.colors.gray_600};
-    padding: 30px;
+    padding: 24px;
 `;
 
 export const Form = styled.View` 
--- src/screens/NewStore/styles.ts
+++ src/screens/NewStore/styles.ts
@@ -5,7 +5,7 @@ import styled from "styled-components/native";
 export const Container = styled(SafeAreaView)`
     flex: 1;
     background-color: ${({theme}) => theme.colors.gray_600};
-    padding: 30px;
+    padding: 24px;
 `;
 
 export const Content = styled.View`
