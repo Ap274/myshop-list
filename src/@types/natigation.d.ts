717725aa34c62ea85653d83def4b3afc0a5325fc
Ap274 <aldopiresaps@gmail.com>
2024-04-17 17:03:40 -0300

    feat: define global type to application routes

---
+++ src/@types/natigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            stores: undefined;
+            new: undefined; 
+            items: {
+                store: string;
+            }
+        }
+    }
+}
