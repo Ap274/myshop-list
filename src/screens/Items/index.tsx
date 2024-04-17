715c221b1d90b66b63abc2f2d35ea2161275c4b1
Ap274 <aldopiresaps@gmail.com>
2024-04-17 14:47:19 -0300

    feat: listing filters on Items screen and a first quantity

--- src/screens/Items/index.tsx
+++ src/screens/Items/index.tsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Caption } from "@components/Caption";
 import { Input } from "@components/Input";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Button } from "@components/Button";
-
-import { Container, Form } from "./styles";
 import { Filter } from "@components/Filter";
 
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+
 export function Items() {
+    const [priority, setPriority] = useState("priority");
+
     return (
         <Container>
            <Header showBackButton/> 
@@ -27,18 +32,26 @@ export function Items() {
                 />
            </Form>
 
-            <Filter 
-                title="Butchers"
-                isActive
-            />
-            <Filter 
-                title="Fruits"
-                isActive
-            />
-            <Filter 
-                title="Dairy Free"
-                isActive
-            />
+            <HeaderList>
+                <FlatList 
+                    data={["priority", "not priority"]}
+                    keyExtractor={item => item}
+                    renderItem={({item}) => (
+                        <Filter 
+                            title={item}
+                            isActive={item === priority} 
+                            onPress={() => setPriority(item)} 
+                        />
+                    )}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                />
+
+                <NumberOfPlayers>
+                    {priority.length}
+                    {/* Change after to Items */}
+                </NumberOfPlayers>
+            </HeaderList>
 
            <Button 
                 title="Remove item"
